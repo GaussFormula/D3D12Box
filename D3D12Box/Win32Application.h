1704e84c8a61b1e55f71c71e49bb860a99ca9e02
GaussFormula <c594794@hotmail.com>
2020-05-09 01:13:41 +0800

    Updated.

--- D3D12Box/Win32Application.h
+++ D3D12Box/Win32Application.h
@@ -6,5 +6,7 @@ class Win32Application
 {
 public:
     static int Run(D3DAppBase* pSample, HINSTANCE hInstance, int nCmdShow);
-
+    static HWND GetHwnd() { return m_hwnd; }
+private:
+    static HWND m_hwnd;
 };
