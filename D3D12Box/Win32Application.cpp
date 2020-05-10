e06d65403dd107fd3834804da5130206d45ebdd1
GaussFormula <c594794@hotmail.com>
2020-05-11 00:05:25 +0800

    Register window class

--- D3D12Box/Win32Application.cpp
+++ D3D12Box/Win32Application.cpp
@@ -5,4 +5,16 @@ int Win32Application::Run(D3DAppBase* pSample, HINSTANCE hInstance, int nCmdShow
     // Parse the command line parameters.
     int argc;
     LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+    pSample->ParseCommandLineArgs(argv, argc);
+    LocalFree(argv);
+
+    // Initialize the window class.
+    WNDCLASSEX windowClass = { 0 };
+    windowClass.cbSize = sizeof(WNDCLASSEX);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = WindowProc;
+    windowClass.hInstance = hInstance;
+    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    windowClass.lpszClassName = L"DXSampleClass";
+    RegisterClassEx(&windowClass);
 }
