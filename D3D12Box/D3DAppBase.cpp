85e630daed0773690074de5113b83b8519c6865d
GaussFormula <c594794@hotmail.com>
2020-06-15 02:22:55 +0800

    Update BuildGeometry

--- D3D12Box/D3DAppBase.cpp
+++ D3D12Box/D3DAppBase.cpp
@@ -485,6 +485,7 @@ void D3DAppBase::BuildGeometry()
     GeometryGenerator::MeshData box = geoGen.CreateBox(1.5f, 0.5f, 1.5f, 3);
     GeometryGenerator::MeshData grid = geoGen.CreateGrid(20.0f, 30.0f, 60, 40);
     GeometryGenerator::MeshData sphere = geoGen.CreateSphere(0.5f, 20, 20);
+    GeometryGenerator::MeshData cylinder = geoGen.CreateCylinder(0.5f, 0.3f, 3.0f, 20, 20);
 
     // We are concatenating all the geometry into a big vertex/index buffer.
     // So define the regions in the buffer each submesh covers.
@@ -500,6 +501,62 @@ void D3DAppBase::BuildGeometry()
     UINT gridIndexOffset = (UINT)box.Indices32.size();
     UINT sphereIndexOffset = gridIndexOffset + (UINT)grid.Indices32.size();
     UINT cylinderIndexOffset = sphereIndexOffset + (UINT)sphere.Indices32.size();
+
+    // Define the SubmeshGeometry that cover different
+    // regions of the vertex/index buffers.
+
+    SubmeshGeometry boxSubmesh;
+    boxSubmesh.IndexCount = (UINT)box.Indices32.size();
+    boxSubmesh.StartIndexLocation = boxIndexOffset;
+    boxSubmesh.BaseVertexLocation = boxVertexOffset;
+
+    SubmeshGeometry gridSubmesh;
+    gridSubmesh.IndexCount = (UINT)grid.Indices32.size();
+    gridSubmesh.StartIndexLocation = gridIndexOffset;
+    gridSubmesh.BaseVertexLocation = gridVertexOffset;
+
+    SubmeshGeometry sphereSubmesh;
+    sphereSubmesh.IndexCount = (UINT)sphere.Indices32.size();
+    sphereSubmesh.StartIndexLocation = sphereIndexOffset;
+    sphereSubmesh.BaseVertexLocation = sphereVertexOffset;
+
+    SubmeshGeometry cylinderSubmesh;
+    cylinderSubmesh.IndexCount = (UINT)cylinder.Indices32.size();
+    cylinderSubmesh.StartIndexLocation = cylinderIndexOffset;
+    cylinderSubmesh.BaseVertexLocation = cylinderVertexOffset;
+
+    // Extract the vertex elements we are interested in and pack the 
+    // vertices of all the meshes into one vertex buffer.
+
+    unsigned int totalVertexCount =
+        box.Vertices.size() +
+        grid.Vertices.size() +
+        sphere.Vertices.size() +
+        cylinder.Vertices.size();
+
+    std::vector<Vertex> vertices(totalVertexCount);
+    UINT k = 0;
+    for (size_t i = 0; i < box.Vertices.size(); i++,k++)
+    {
+        vertices[k].position = box.Vertices[i].Position;
+        vertices[k].color = XMFLOAT4(DirectX::Colors::DarkGreen);
+    }
+    for (size_t i = 0; i < grid.Vertices.size(); i++,k++)
+    {
+        vertices[k].position = grid.Vertices[i].Position;
+        vertices[k].color = XMFLOAT4(DirectX::Colors::ForestGreen);
+    }
+    for (size_t i = 0; i < sphere.Vertices.size(); i++,k++)
+    {
+        vertices[k].position = sphere.Vertices[i].Position;
+        vertices[k].color = XMFLOAT4(DirectX::Colors::SteelBlue);
+    }
+
+    std::vector<std::uint16_t> indices;
+    indices.insert(indices.end(), std::begin(box.GetIndices16()), std::end(box.GetIndices16()));
+    indices.insert(indices.end(), std::begin(grid.GetIndices16()), std::end(grid.GetIndices16()));
+    indices.insert(indices.end(), std::begin(sphere.GetIndices16()), std::end(sphere.GetIndices16()));
+    indices.insert(indices.end(), std::begin(cylinder.GetIndices16()), std::end(cylinder.GetIndices16()));
 }
 
 void D3DAppBase::BuildConstantDescriptorHeaps()
